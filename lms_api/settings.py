981534f124482d020df38268cb29557112a60a11
Hiren Rupchandani <rupchandanihiren@gmail.com>
2023-04-23 14:30:58 -0400

    Updated files

--- lms_api/settings.py
+++ lms_api/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
